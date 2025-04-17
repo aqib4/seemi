95de24f61c8d0449a0aef68eac2744c245314175
app <app@apps-MacBook-Pro.local>
2025-04-16 22:51:21 -0700

    bug fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/Footer.css";
+import "../styles/footer.css";
 import logo from "../assets/logo.png";
 import { MdEmail } from "react-icons/md";
 import { HiPhone } from "react-icons/hi2";
